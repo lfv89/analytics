9c556b25c0f307557d5e08e43447e7ece13bc9c3
Luis Vasconcellos <luis@luisvasconcellos.com>
2020-06-01 22:19:45 -0300

    Add Elastic and Kibana

---
+++ go.mod
@@ -0,0 +1,5 @@
+module analytics
+
+require github.com/elastic/go-elasticsearch/v7 v7.0.0
+
+go 1.12
--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
-func handler(conn net.Conn) {
-	var err error
-	var req *http.Request
+type Event struct {
+	Source    string `json:"source"`
+	UserAgent string `json:"userAgent"`
+}
 
+func handler(conn net.Conn) {
 	buf := bufio.NewReader(conn)
-	req, err = http.ReadRequest(buf)
+	req, err := http.ReadRequest(buf)
 
 	if err == io.EOF {
 		log.Println("Client disconnected")
 	}
 
 	if err != nil {
-		log.Println("Unexpected error")
+		log.Println("An Unexpected error ocurred")
 	}
 
+	rawBody := &Event{
+		Source:    req.Referer(),
+		UserAgent: req.UserAgent(),
+	}
+
+	body, _ := json.Marshal(rawBody)
+	es, _ := elasticsearch.NewDefaultClient()
+	res, err := es.Index("events", bytes.NewReader(body))
+
+	fmt.Println(res)
+	fmt.Println(err)
+
 	response := []byte("HTTP/1.1 200 OK\nContent-Type: image/jpeg\nContent-Length: 3\nAccess-Control-Allow-Origin: *\n\nimg")
-
-	fmt.Println("Read:")
-	fmt.Println(req.URL)
-	fmt.Println(req.UserAgent())
-
-	fmt.Println("Writing...")
 	if _, err := conn.Write(response); err != nil {
 		log.Fatalln("Unable to write data")
 	}
@@ -40,22 +52,10 @@ func handler(conn net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":4001")
-
-	if err != nil {
-		log.Println("Unable to bind to port")
-	}
-
-	log.Println("Listening to 0.0.0.0:4001")
+	listener, _ := net.Listen("tcp", ":4001")
 
 	for {
-		conn, err := listener.Accept()
-		log.Println("Received connection")
-
-		if err != nil {
-			log.Println("Unable to accept connection")
-		}
-
+		conn, _ := listener.Accept()
 		go handler(conn)
 	}
 }
